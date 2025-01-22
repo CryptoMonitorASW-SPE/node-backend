6e90c0ca10a742df7dc3e5f8b42a91f6b88e9dd6
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-23 00:55:28 +0100

    refactor: integrate DomainEventDispatcher, update constructor, and enhance isValidEventData

--- app/src/application/EventService.ts
+++ app/src/application/EventService.ts
@@ -1,29 +1,42 @@
+// EventService.ts
+import { container } from 'tsyringe';
 import { injectable, inject } from 'tsyringe';
-import { EventInputPort } from '../application/ports/EventInputPort';
-import { EventOutputPort } from '../application/ports/EventOutputPort';
-import { Event } from '../domain/model/Event';
+import { EventInputPort } from './ports/EventInputPort';
+import { DomainEventDispatcher, EventDispatcher } from '../domain/services/EventDispatcher';
+import { Event, EventType } from '../domain/model/Event';
+import { CryptoUpdateHandler } from '../domain/services/CryptoUpdateHandler';
+import { EventHandler } from '../domain/model/EventHandler';
+import { EventOutputPort } from './ports/EventOutputPort'; // Ensure this import exists
 
 @injectable()
 export class EventService implements EventInputPort {
+    private eventDispatcher: EventDispatcher;
+
     constructor(
-        @inject('EventOutputPort') private eventOutput: EventOutputPort
-    ) {}
+        @inject('EventOutputPort') private eventOutputPort: EventOutputPort
+    ) {
+        const cryptoUpdateHandler = new CryptoUpdateHandler(this.eventOutputPort);
+        // Initialize other handlers here
+        const handlers: EventHandler[] = [cryptoUpdateHandler /*, other handlers */];
+        const eventDispatcher = new DomainEventDispatcher(handlers);
+        container.registerInstance<DomainEventDispatcher>('EventDispatcher', eventDispatcher);
+        this.eventDispatcher = eventDispatcher; // Assigning to the class property
+    }
 
     async processEvent(eventJson: Event): Promise<void> {
-        
         if (!this.isValidEventData(eventJson)) {
             throw new Error('Invalid event data');
         }
 
-        const messageJson = {
-            ...eventJson,
-            timestamp: new Date().toISOString()
-        };
-
-        this.eventOutput.broadcast(messageJson);
+        this.eventDispatcher.dispatch(eventJson);
     }
 
     private isValidEventData(eventJson: Event): boolean {
-        return !!(eventJson && eventJson.eventType && eventJson.payload);
+        const validTypes: EventType[] = Object.values(EventType); // Dynamic retrieval of valid types
+        return (
+            !!eventJson &&
+            validTypes.includes(eventJson.eventType) &&
+            eventJson.payload.length > 0
+        );
     }
 }
